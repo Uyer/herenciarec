06a506f56a91ed04dda4aece76e31e124b4a3fc1
Mrtn <martin.teysera@gmail.com>
2020-05-06 14:48:22 +0200

    Add Font Awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap")
-require("@fortawesome/fontawesome-free")
+// require("@fortawesome/fontawesome-free")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
